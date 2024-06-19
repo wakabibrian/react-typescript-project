66dd8b476233457fe51e0d6fd24b715c06b9a168
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-19 22:18:32 +0300

    feat: Add Generics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Heading from "./components/Heading";
 import Section from "./components/Section";
 import Counter from "./components/Counter";
+import List from "./components/List";
 
 import { useState } from "react";
 
@@ -12,6 +13,10 @@ function App() {
       <Heading title="Hello" />
       <Section title="Different Title">This is my section</Section>
       <Counter setCount={setCount}>The Count is {count}</Counter>
+      <List
+        items={["🍵 Coffee", "🌮 Tacos", "💻 Code"]}
+        render={(item: string) => <span className="gold">{item}</span>}
+      />
     </>
   );
 }
