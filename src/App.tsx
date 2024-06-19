13584983011645cbc756f899cee62a99505901fe
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-19 21:46:01 +0300

    feat: Add Counter example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Heading from "./components/Heading";
 import Section from "./components/Section";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <>
       <Heading title="Hello" />
       <Section title="Different Title">This is my section</Section>
+      <Counter />
     </>
   );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState<number>(1);
-  return <div>Counter</div>;
+  return (
+    <>
+      <h1>Count is {count}</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+      <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+    </>
+  );
 };
 
 export default Counter;
