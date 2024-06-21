6e836bb0cfcc872320a6fce576529f94d868d775
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-21 22:40:09 +0300

    feat: Add useState Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import Heading from "./components/Heading";
 import Section from "./components/Section";
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
 import List from "./components/List";
 
 import { useState } from "react";
 
+interface User {
+  id: number;
+  username: string;
+}
+
 function App() {
-  const [count, setCount] = useState<number>(1);
+  // const [count, setCount] = useState<number>(1);
+  const [count, setCount] = useState<number>(0);
+  const [user, setUser] = useState<User[] | null>(null);
 
   return (
     <>
       <Heading title="Hello" />
       <Section title="Different Title">This is my section</Section>
-      <Counter setCount={setCount}>The Count is {count}</Counter>
+      {/* <Counter setCount={setCount}>The Count is {count}</Counter> */}
       <List
         items={["🍵 Coffee", "🌮 Tacos", "💻 Code"]}
         render={(item: string) => <span className="gold">{item}</span>}
