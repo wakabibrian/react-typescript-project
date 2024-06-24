59bc734cc3107ad8b5ca70a5848fb0355f504a3b
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-24 05:57:16 +0300

    feat: Add  Payload

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,16 @@
-import { ReactNode, useReducer } from "react";
+import { ChangeEvent, ReactNode, useReducer } from "react";
 
-const initState = { count: 0 };
+const initState = { count: 0, text: "" };
 
 const enum REDUCER_ACTION_TYPE {
   INCREMENT,
   DECREMENT,
+  NEW_INPUT,
 }
 
 type ReducerAction = {
   type: REDUCER_ACTION_TYPE;
+  payload?: string;
 };
 
 const reducer = (
@@ -20,6 +22,8 @@ const reducer = (
       return { ...state, count: state.count + 1 };
     case REDUCER_ACTION_TYPE.DECREMENT:
       return { ...state, count: state.count - 1 };
+    case REDUCER_ACTION_TYPE.NEW_INPUT:
+      return { ...state, text: action.payload ?? "" };
     default:
       throw new Error();
   }
@@ -34,12 +38,19 @@ const Counter = ({ children }: ChildrenType) => {
 
   const increment = () => dispatch({ type: REDUCER_ACTION_TYPE.INCREMENT });
   const decrement = () => dispatch({ type: REDUCER_ACTION_TYPE.DECREMENT });
+  const handleTextInput = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: REDUCER_ACTION_TYPE.NEW_INPUT, payload: e.target.value });
+  };
 
   return (
     <>
       <h1>{children(state.count)}</h1>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
+      <div>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
+      <input type="text" onChange={handleTextInput} />
+      <h2>{state.text}</h2>
     </>
   );
 };
