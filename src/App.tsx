5b2b3929e89a710c0a86f7f87e6be4565e16658a
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-24 05:12:56 +0300

    chore(new lesson): Create starting files - useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,74 @@
 // import Heading from "./components/Heading";
 // import Section from "./components/Section";
-// import Counter from "./components/Counter";
+import { useState } from "react";
+import Counter from "./components/Counter";
 // import List from "./components/List";
 
-import {
-  useEffect,
-  useState,
-  useCallback,
-  useMemo,
-  useRef,
-  MouseEvent,
-  KeyboardEvent,
-} from "react";
+// import {
+//   useEffect,
+//   useState,
+//   useCallback,
+//   useMemo,
+//   useRef,
+//   MouseEvent,
+//   KeyboardEvent,
+// } from "react";
 
-interface User {
-  id: number;
-  username: string;
-}
+// interface User {
+//   id: number;
+//   username: string;
+// }
 
-type fibFunc = (n: number) => number;
+// type fibFunc = (n: number) => number;
 
-const fib: fibFunc = (n) => {
-  if (n < 2) return n;
-  return fib(n - 1) + fib(n - 2);
-};
+// const fib: fibFunc = (n) => {
+//   if (n < 2) return n;
+//   return fib(n - 1) + fib(n - 2);
+// };
 
-const myNum: number = 37;
+// const myNum: number = 37;
 
 function App() {
-  // const [count, setCount] = useState<number>(1);
-  const [count, setCount] = useState<number>(0);
-  const [users, setUsers] = useState<User[] | null>(null);
+  const [count, setCount] = useState<number>(1);
+  // const [count, setCount] = useState<number>(0);
+  // const [users, setUsers] = useState<User[] | null>(null);
 
-  const inputRef = useRef<HTMLInputElement>(null);
+  // const inputRef = useRef<HTMLInputElement>(null);
 
-  console.log(inputRef?.current);
-  console.log(inputRef?.current?.value);
+  // console.log(inputRef?.current);
+  // console.log(inputRef?.current?.value);
 
-  useEffect(() => {
-    console.log("Mounting");
-    console.log("Users: ", users);
+  // useEffect(() => {
+  //   console.log("Mounting");
+  //   console.log("Users: ", users);
 
-    return () => {
-      console.log("unmounting");
-    };
-  }, [users]);
+  //   return () => {
+  //     console.log("unmounting");
+  //   };
+  // }, [users]);
 
-  const addTwo = useCallback(
-    (
-      e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>
-    ): void => setCount((prev) => prev + 1),
-    []
-  ); //Doesn't call the function again (Memorizes a function)
+  // const addTwo = useCallback(
+  //   (
+  //     e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>
+  //   ): void => setCount((prev) => prev + 1),
+  //   []
+  // ); //Doesn't call the function again (Memorizes a function)
 
-  const result = useMemo<number>(() => fib(myNum), [myNum]); //Memorizes a value or the result value (first time it will take long to calculate then it will memorize, for expensive calculation)
+  // const result = useMemo<number>(() => fib(myNum), [myNum]); //Memorizes a value or the result value (first time it will take long to calculate then it will memorize, for expensive calculation)
 
   return (
     <>
       {/* <Heading title="Hello" /> */}
       {/* <Section title="Different Title">This is my section</Section> */}
-      {/* <Counter setCount={setCount}>The Count is {count}</Counter> */}
+      <Counter setCount={setCount}>The Count is {count}</Counter>
       {/* <List
         items={["🍵 Coffee", "🌮 Tacos", "💻 Code"]}
         render={(item: string) => <span className="gold">{item}</span>}
       /> */}
-      <h1>{count}</h1>
+      {/* <h1>{count}</h1>
       <button onClick={addTwo}>Add</button>
       <h2>{result}</h2>
-      <input ref={inputRef} type="text" />
+      <input ref={inputRef} type="text" /> */}
     </>
   );
 }
